1738bd801c80a138249bcb9bd7df6a9fe2a38cfa
Hércules Ramlow <63937242+HRsniper@users.noreply.github.com>
2021-06-22 13:46:04 -0400

    a41

---
+++ aula41-Nerves.ex
@@ -0,0 +1,19 @@
+# O projeto Nerves é um framework para utilizar Elixir em desenvolvimento de software embarcado.
+# Como o website do Nerves diz, ele permite que você “construa e faça deploy de software embarcado
+# à prova de balas em Elixir”.
+# Nerves é um pouco mais difícil de absorver já que requer a configuração de um sistema avançado
+# e hardware adicional, então pode não ser adequado para iniciantes.
+
+# Para escrever código embarcado usando Nerves, você vai precisar de um dos dispositivos suportados (https://hexdocs.pm/nerves/targets.html#supported-targets-and-systems),
+# um leitor de cartão com um cartão de memória suportado pelo hardware de sua escolha,
+# e uma conexão a rede cabeada para acessar esse dispositivo pela rede.
+
+# O sugerido para usar um Raspberry Pi, já que ele tem um LED controlável onboard.
+# Também é recomendável ter uma tela conectada ao seu dispositivo alvo já que isso vai simplificar
+# o debug usando IEx.
+
+https://elixirschool.com/pt/lessons/specifics/nerves/
+https://hexdocs.pm/nerves/getting-started.html
+https://github.com/nerves-project/nerves
+https://www.nerves-project.org/
+https://youtu.be/IU8epH8FAI4?t=36
